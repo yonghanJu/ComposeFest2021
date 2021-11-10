cdd3a60cb3d2017308f96ff1a644801fde7a6631
yonghanJu <ju990828@naver.com>
2021-11-10 21:32:20 +0900

    Update MainActivity.kt

--- week 1-Jetpack Compose basics/DevFest_1/app/src/main/java/com/jyh/devfest_1/MainActivity.kt
+++ week 1-Jetpack Compose basics/DevFest_1/app/src/main/java/com/jyh/devfest_1/MainActivity.kt
@@ -3,11 +3,19 @@ package com.jyh.devfest_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jyh.devfest_1.ui.theme.DevFest_1Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,22 +25,100 @@ class MainActivity : ComponentActivity() {
             DevFest_1Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
+
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier
+                    .padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    DevFest_1Theme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+@Composable
+fun MyApp(){
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun Greetings(names: List<String> = List(1000) { "$it" }){
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)){
+        items(items = names) {
+            Greeting(name = it)
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ){
+        Row(modifier = Modifier.padding(24.dp)){
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+            ) {
+                Text(text = "Hello,")
+                Text(text = "$name!", style = MaterialTheme.typography.h4)
+            }
+            OutlinedButton(onClick = { expanded = !expanded }) {
+                Text(if(!expanded) "Show more" else "Hide")
+            }
+        }
+
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     DevFest_1Theme {
-        Greeting("Android")
+        MyApp()
     }
 }
