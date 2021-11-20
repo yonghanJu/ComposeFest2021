312ecce50808c98126ce4532b0cbedc996b0eb11
yonghanJu <ju990828@naver.com>
2021-11-21 00:35:32 +0900

    week 3-1

--- week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
+++ week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -1,13 +1,23 @@
 package com.codelab.theming.ui.start.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 @Composable
-fun JetnewsTheme(content: @Composable () ->Unit){
-    MaterialTheme(content = content)
+fun JetnewsTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = if (darkTheme) DarkColors else LightColors,
+        typography = JetnewsTypography,
+        shapes = JetnewsShapes,
+        content = content
+    )
 }
 
 private val LightColors = lightColors(
@@ -18,4 +28,13 @@ private val LightColors = lightColors(
     secondaryVariant = Red900,
     onSecondary = Color.White,
     error = Red800
+)
+
+private val DarkColors = darkColors(
+    primary = Red300,
+    primaryVariant = Red700,
+    onPrimary = Color.Black,
+    secondary = Red300,
+    onSecondary = Color.Black,
+    error = Red200
 )
